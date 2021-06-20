df849b95ad32338749410805f86f4aa12daa9d75
fernaned <fernandoed.bm@gmail.com>
2021-06-20 16:45:28 -0500

    chapter 8 dome

--- chapter7/a_robot.js
+++ chapter7/a_robot.js
@@ -239,34 +239,6 @@ function compareRobots(robot1, memory1, robot2, memory2) {
   `);
 }
 
-// compareRobots(ferBot, undefined, randomRobot, []);
-// compareRobots(ferBot, undefined, routeRobot, []);
-// compareRobots(ferBot, undefined, goalOrientedRobot, []);
-
-function superDuperRobot({ place, parcels }, route) {
-	if (route.length == 0) {
-		let pickUpRoutes = [];
-		let deliveryRoutes = [];
-
-		for (const parcel of parcels) {
-			if (parcel.place != place) {
-				pickUpRoutes.push(findRoute(roadGraph, place, parcel.place));
-			} else {
-				deliveryRoutes.push(findRoute(roadGraph, place, parcel.address));
-			}
-		}
-
-		pickUpRoutes.sort((a, b) => {
-			return a.length - b.length;
-		});
-
-		deliveryRoutes.sort((a, b) => {
-			return a.length - b.length;
-		});
-
-		route = pickUpRoutes.length == 0 ? deliveryRoutes[0] : pickUpRoutes[0];
-	}
-	return { direction: route[0], memory: route.slice(1) };
-}
-
-compareRobots(ferBot, undefined, superDuperRobot, []);
+compareRobots(ferBot, undefined, randomRobot, []);
+compareRobots(ferBot, undefined, routeRobot, []);
+compareRobots(ferBot, undefined, goalOrientedRobot, []);
